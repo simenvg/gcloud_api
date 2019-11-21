707624ae73b88554277c36039c6629a09f77b9b4
GRINI Simen <simen.grini@soprasteria.com>
2019-11-21 08:08:40 +0100

    auto deploy test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-container = ['Hello World!', 'Google']
+container = ['Hello World!', 'Google', "SIMEN"]
 
 @app.route('/mainpage/')
 def mainpage():
